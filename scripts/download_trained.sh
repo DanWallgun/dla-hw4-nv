fbd42dd11a6740efa7c9f4f7adfda182305515f8
Daniil Volgin <DanWallgun@github.com>
2022-12-22 23:39:40 +0300

    add final results and ckpts

--- scripts/download_trained.sh
+++ scripts/download_trained.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$1" == "full" ]; then
-    # full state https://drive.google.com/file/d/1E3tgnRApfl0RbAtbsNC7zaQxjVxy0szm/view?usp=sharing
+    # full state https://drive.google.com/file/d/1gZTGZKnWgQVPaRD5wEmuD5igRSDhHOEH/view?usp=sharing
     gdown \
-        "https://drive.google.com/u/0/uc?id=1E3tgnRApfl0RbAtbsNC7zaQxjVxy0szm&confirm=yes" \
+        "https://drive.google.com/u/0/uc?id=1gZTGZKnWgQVPaRD5wEmuD5igRSDhHOEH&confirm=yes" \
         -O ./ckpts/full_checkpoint.dict
 else
-    # converted (only generator) https://drive.google.com/file/d/1j6BdVOL5aQTpUb_reWInoUpfkvby3Njc/view?usp=sharing
+    # converted (only generator) https://drive.google.com/file/d/1bI35lkfBiNIYN5Uf6DOJsjczNzlN7Vjh/view?usp=share_link
     gdown \
-        "https://drive.google.com/u/0/uc?id=1j6BdVOL5aQTpUb_reWInoUpfkvby3Njc&confirm=yes" \
+        "https://drive.google.com/u/0/uc?id=1bI35lkfBiNIYN5Uf6DOJsjczNzlN7Vjh&confirm=yes" \
         -O ./infer_checkpoint.dict
 fi
